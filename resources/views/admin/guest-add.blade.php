016a49a7f50a6c8839fcf2e7ccb1a818b76592b8
abyl@kz.kz <abyl@kz.kz>
2021-06-10 02:01:14 +0500

    fixed guest add

--- resources/views/admin/guest-add.blade.php
+++ resources/views/admin/guest-add.blade.php
@@ -29,8 +29,13 @@
                                         <div class="form-group">
                                             <label for="name" class="form-control-label">ФИО</label>
 
-                                            <input id="name" type="text" class="form-control" name="name" value="" placeholder="Введите ФИО" >
+                                            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" placeholder="Введите ФИО" >
 
+                                            @if ($errors->has('name'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
@@ -42,7 +47,11 @@
                                                     <option value="{{ $item->id }}"> {{ $item->name }}</option>
                                                 @endforeach
                                             </select>
-
+                                            @if ($errors->has('user_id'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('user_id') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
                                     <div class="col-md-12">
@@ -55,6 +64,11 @@
                                                 <option value="apec">Apec Petrotechnic</option>
 
                                             </select>
+                                            @if ($errors->has('location'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('location') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
 
                                     </div>
@@ -62,8 +76,13 @@
                                         <div class="form-group">
                                             <label for="passport" class="form-control-label">ИИН</label>
 
-                                            <input id="passport" type="number" class="form-control" name="passport" value="" placeholder="Введите ИИН" >
+                                            <input id="passport" type="number" class="form-control{{ $errors->has('passport') ? ' is-invalid' : '' }}" name="passport" value="{{ old('passport') }}" placeholder="Введите ИИН" >
 
+                                            @if ($errors->has('passport'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('passport') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
@@ -71,8 +90,13 @@
                                         <div class="form-group">
                                             <label for="phone" class="form-control-label">Номер телефона</label>
 
-                                            <input id="phone" type="text" class="form-control" name="phone" value="" placeholder="Введите номер телефона" >
+                                            <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" placeholder="Введите номер телефона" >
 
+                                            @if ($errors->has('phone'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('phone') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
@@ -80,8 +104,13 @@
                                         <div class="form-group">
                                             <label for="room" class="form-control-label">Комната</label>
 
-                                            <input id="room" type="text" class="form-control" name="room" value="" placeholder="Номер комнаты" >
+                                            <input id="room" type="text" class="form-control{{ $errors->has('room') ? ' is-invalid' : '' }}" name="room" value="{{ old('room') }}" placeholder="Номер комнаты" >
 
+                                            @if ($errors->has('room'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('room') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
@@ -95,6 +124,11 @@
                                                 <option value="Гостиница полулюкс">Гостиница полулюкс</option>
                                                 <option value="Гостиница люкс">Гостиница люкс</option>
                                             </select>
+                                            @if ($errors->has('room_type'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('room_type') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
 
                                     </div>
@@ -103,8 +137,13 @@
                                         <div class="form-group">
                                             <label for="entry" class="form-control-label">Въезд</label>
 
-                                            <input id="entry" type="datetime-local" class="form-control" name="entry" value="" >
+                                            <input id="entry" type="datetime-local" class="form-control{{ $errors->has('entry') ? ' is-invalid' : '' }}" name="entry" value="{{ old('entry') }}" >
 
+                                            @if ($errors->has('entry'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('entry') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
@@ -112,8 +151,13 @@
                                         <div class="form-group">
                                             <label for="departure" class="form-control-label">Отъезд</label>
 
-                                            <input id="departure" type="datetime-local" class="form-control" name="departure" value="" >
+                                            <input id="departure" type="datetime-local" class="form-control{{ $errors->has('departure') ? ' is-invalid' : '' }}" name="departure" value="{{ old('departure') }}" >
 
+                                            @if ($errors->has('departure'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('departure') }}</strong>
+                                                </span>
+                                            @endif
                                         </div>
                                     </div>
 
